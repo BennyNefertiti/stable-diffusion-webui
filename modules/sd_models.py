72f7d8d726153d77166716ae2201bda0272fe37e
Bernardo Alves <bernardo@nefertiti.tech>
2025-01-24 15:44:54 +0000

    Change model hash function

--- modules/sd_models.py
+++ modules/sd_models.py
@@ -192,19 +192,14 @@ def get_closet_checkpoint_match(search_string):
 
 
 def model_hash(filename):
-    """old hash that only looks at a small part of the file and is prone to collisions"""
-
     try:
         with open(filename, "rb") as file:
             import hashlib
             m = hashlib.sha256()
-
-            file.seek(0x100000)
             m.update(file.read(0x10000))
             return m.hexdigest()[0:8]
-    except FileNotFoundError:
-        return 'NOFILE'
-
+    except:
+        return "0" * 8
 
 def select_checkpoint():
     """Raises `FileNotFoundError` if no checkpoints are found."""
